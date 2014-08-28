c0d6e59ec25299f5346f2061b9dba09e0d3cd9a7
Sébastien Muller <smuller@tequilarapido.com>
2014-08-28 10:03:46 +0200

    Setup infoWindow + markers references

--- google-map.js
+++ google-map.js
@@ -3,11 +3,15 @@
     'use strict';
 
     var googleMap = (function () {
+
         function googleMap(element, options) {
             this.map = new google.maps.Map(element, options);
+
+            this.markers = [];
         }
 
         googleMap.prototype = {
+
             zoom: function (level) {
                 if (level) {
                     this.map.setZoom(level);
@@ -16,21 +20,16 @@
                 }
             },
 
-            _on: function (options) {
-                // Listen event and run callback in the context of this instance
-                var self = this;
-                google.maps.event.addListener(options.element, options.event, function (event) {
-                    options.callback.call(self, event);
-                });
-            },
-
             addMarker: function (options) {
+                var self = this;
+
                 options.position = {
                     lat: options.lat,
                     lng: options.lng
                 };
 
                 var marker = this._createMarker(options);
+
                 if (options.event) {
                     this._on({
                         element: marker,
@@ -38,16 +37,63 @@
                         callback: options.event.callback
                     });
                 }
+
+                if (options.content) {
+                    this._on({
+                        element: marker,
+                        event: 'click',
+                        callback: function () {
+                            var infoWindow = new google.maps.InfoWindow({
+                                content: '<div style="height: 200px;">' + options.content + '</div>'
+                            });
+                            infoWindow.open(self.map, marker);
+                        }
+                    });
+                }
+
+                return marker;
+            },
+
+            delMarker: function (marker) {
+                var indexOf = this.markers.indexOf(marker);
+                if (indexOf !== -1) {
+                    this.markers.splice(indexOf, 1);
+                    marker.setMap(null);
+                }
+            },
+
+            findMarkerByLat: function (lat) {
+                for (var i = 0 ; i < this.markers.length ; i++) {
+                    var marker = this.markers[i];
+                    if (marker.position.lat() === lat) {
+                        return marker;
+                    }
+                }
+
+                return false;
             },
 
             _createMarker: function (options) {
                 options.map = this.map;
 
-                return new google.maps.Marker(options);
+                var marker = new google.maps.Marker(options);
+                this.markers.push(marker);
+
+                return marker;
+            },
+
+            _on: function (options) {
+                // Listen event and run callback in the context of this instance
+                var self = this;
+                google.maps.event.addListener(options.element, options.event, function (event) {
+                    options.callback.call(self, event);
+                });
             }
+
         };
 
         return googleMap;
+        
     }());
 
     googleMap.create = function (element, options) {
--- scripts.js
+++ scripts.js
@@ -8,22 +8,37 @@
     // Actual map object
     var map = googleMap.create(element, googleMap.MAP_OPTIONS);
 
-    map.zoom(18);
-    map.addMarker({
+    map.zoom(15);
+
+    var marker1 = map.addMarker({
         lat: 37.791350,
         lng: -122.435883,
         icon: 'markers/fire.png', // http://mapicons.nicolasmollet.com/
         draggable: true,
         // Custom options
         id: 1,
+        content: '<div style="">I like food</div>'
+    });
+
+    var marker2 = map.addMarker({
+        lat: 37.781350,
+        lng: -122.455883,
+        icon: 'markers/blast.png', // http://mapicons.nicolasmollet.com/
+        draggable: true,
+        // Custom options
+        id: 2,
         event: {
             name: 'click',
-            callback: function (event) {
+            callback: function (evt) {
                 alert('Clicked!');
-                console.log(event);
+                console.log(evt);
                 console.log(this);
             }
         }
     });
+    map.delMarker(marker2);
+
+    var found = map.findMarkerByLat(37.791350);
+    console.log(found);
     
 }(window, window.googleMap));
